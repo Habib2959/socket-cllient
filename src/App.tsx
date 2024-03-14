89c287c0aa936b482f5fab00ad72433da0a60324
Habib2959 <rahman.habibjr59@gmail.com>
2024-03-14 14:35:25 +0600

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import "./App.css";
 
 function App() {
 	const messageRef = useRef<HTMLInputElement>(null);
-	const socket = io("https://socket-server-fuve.onrender.com/");
+	const socket = io("https://socket-server-fuve.onrender.com/", {
+		withCredentials: true,
+	});
 
 	const sendMesage = (e: React.FormEvent) => {
 		e.preventDefault();
